8631c362d9276da5a589ff5ba89f5816b923a195
Rosen Blagoev <rosen.blagoev@linkmobility.com>
2023-02-12 18:32:47 +0200

    beer data

---
+++ src/main/java/com/example/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.example.msscbeerservice.bootstrap;
+
+import com.example.msscbeerservice.domain.Beer;
+import com.example.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337723423425233L)
+                    .price(new BigDecimal("12.56"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(23)
+                    .upc(337723423425234L)
+                    .price(new BigDecimal("11.6"))
+                    .build());
+
+        }
+
+    }
+}
--- src/main/java/com/example/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/com/example/msscbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,6 @@
 package com.example.msscbeerservice.repositories;
 
 import com.example.msscbeerservice.domain.Beer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
