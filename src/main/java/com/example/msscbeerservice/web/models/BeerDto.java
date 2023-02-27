cd584e042dc24ff2453eac984f851cf264a3c6d7
Rosen Blagoev <rosen.blagoev@linkmobility.com>
2023-02-27 20:24:07 +0200

    JSON Format

--- src/main/java/com/example/msscbeerservice/web/models/BeerDto.java
+++ src/main/java/com/example/msscbeerservice/web/models/BeerDto.java
@@ -1,5 +1,6 @@
 package com.example.msscbeerservice.web.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,8 +24,12 @@ public class BeerDto {
 
     @Null
     private Integer version;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
 
@@ -35,6 +40,7 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     private BigDecimal price;
     private Integer quantityAtHand;
